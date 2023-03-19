94442c863afc4215e7c4975102378d4a0a383ef4
Murtaza0726 <murtazanaqvi0726@yahoo.com>
2023-03-19 16:02:27 -0400

    added field validation and user sign

--- al-zehraApp/ViewController.swift
+++ al-zehraApp/ViewController.swift
@@ -6,18 +6,57 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet var emailText: UITextField!
+    @IBOutlet var passwordText: UITextField!
+    @IBOutlet var invalidEmailText: UILabel!
+    @IBOutlet var invalidPasswordText: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        invalidEmailText.isEnabled = false
+        invalidPasswordText.isEnabled = false
+        
         // Do any additional setup after loading the view.
     }
 
+    
     @IBAction func registerBtn(_ sender: UIButton) {
         let registerPageVC = self.storyboard?.instantiateViewController(withIdentifier: "signUpViewController") as! signUpViewController
         self.navigationController?.pushViewController(registerPageVC, animated: true)
     }
     
+    @IBAction func signInBtn(_ sender: UIButton) {
+        signInUserfunc()
+    }
+    
+    //function to sign in
+    func signInUserfunc(){
+        Auth.auth().signIn(withEmail: emailText.text ?? "", password: passwordText.text ?? "") { firebaseResult, error in
+            if let e = error {
+                print(e.localizedDescription)
+                print("not working")
+                self.invalidEmailText.text = "Please enter valid email"
+                self.invalidPasswordText.text = "Please enter valid password"
+                self.invalidEmailText.isEnabled = true
+                self.invalidPasswordText.isEnabled = true
+                self.invalidEmailText.textColor = UIColor.red
+                self.invalidPasswordText.textColor = UIColor.red
+                
+            }else{
+                
+                //navigate to app home page
+                let homePageVC = self.storyboard?.instantiateViewController(withIdentifier: "homeViewController") as! homeViewController
+                self.navigationController?.pushViewController(homePageVC, animated: true)
+            }
+        }
+    }
 }
 
--- al-zehraApp/signUpViewController.swift
+++ al-zehraApp/signUpViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class signUpViewController: UIViewController {
 
@@ -16,20 +17,127 @@ class signUpViewController: UIViewController {
     @IBOutlet var phoneNumText: UITextField!
     @IBOutlet var emailText: UITextField!
     @IBOutlet var passwordText: UITextField!
+    @IBOutlet var invalidEmailMsg: UILabel!
+    @IBOutlet var invalidPhoneNum: UILabel!
+    @IBOutlet var invalidPassword: UILabel!
+    @IBOutlet var invalidFirstName: UILabel!
+    @IBOutlet var invalidLastName: UILabel!
+    
+    //database reference
     
     var ref = DatabaseReference.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        invalidEmailMsg.isEnabled = false
+        invalidPassword.isEnabled = false
+        invalidPhoneNum.isEnabled = false
+        invalidLastName.isEnabled = false
+        invalidFirstName.isEnabled = false
         self.ref = Database.database().reference()
         
     }
     
+    //button to sign up
     @IBAction func signUpBtn(_ sender: UIButton) {
+        self.createUserfunc()
         self.saveData()
+        self.emailValidate()
+        self.phoneValidate()
+        self.passValidate()
+        self.validFirstName()
+        self.validLastName()
     }
+    
+    //function to save user info to database
     
     func saveData(){
         let dict = ["firstName":fNameText.text!, "LastName":LastNameText.text!, "phone":phoneNumText.text!, "email":emailText.text!, "password":passwordText.text!]
         self.ref.child("userDetails").childByAutoId().setValue(dict)
+    }
+    
+    //function to ccreate user for login
+    
+    func createUserfunc(){
+        Auth.auth().createUser(withEmail: emailText.text ?? "", password: passwordText.text ?? "") { firebaseResult, error in
+            if let e = error {
+                print(e.localizedDescription)
+                print("not working")
+            }else{
+                
+                //navigate to app home page
+                let homePageVC = self.storyboard?.instantiateViewController(withIdentifier: "homeViewController") as! homeViewController
+                self.navigationController?.pushViewController(homePageVC, animated: true)
+            }
+        }
+    }
+    //function to validate email address field
+    func emailValidate(){
+        if (emailText.text?.isValidEmail)!{
+            print("your email is valid")
+        }
+        else{
+            print("email is invalid")
+            invalidEmailMsg.isEnabled = true
+            invalidEmailMsg.text = "Invalid email !!"
+            invalidEmailMsg.textColor = UIColor.red
+        }
+    }
+    
+    //function to validate phone number field
+    func phoneValidate(){
+        if (phoneNumText.text?.isValidPhoneNumber)!{
+            print("your phone number is valid")
+        }else{
+            print("phone is invalid")
+            invalidPhoneNum.isEnabled = true
+            invalidPhoneNum.text = "Invalid phone number !!"
+            invalidPhoneNum.textColor = UIColor.red
+        }
+    }
+    
+    //function to validate password field
+    func passValidate(){
+        if (passwordText.text?.isValidPassword)!{
+            print("your password is valid")
+        }else{
+            print("password is invalid")
+            invalidPassword.isEnabled = true
+            invalidPassword.text = "Invalid password!!"
+            invalidPassword.textColor = UIColor.red
+        }
+    }
+    func validFirstName(){
+        if(fNameText.text?.isEmpty)!{
+            self.invalidFirstName.isEnabled = true
+            self.invalidFirstName.textColor = UIColor.red
+            self.invalidFirstName.text = "Field can not be Empty"
+        }
+    }
+    func validLastName(){
+        if(LastNameText.text?.isEmpty)!{
+            self.invalidLastName.isEnabled = true
+            self.invalidLastName.textColor = UIColor.red
+            self.invalidLastName.text = "PField can not be Empty"
+        }
+    }
+}
+
+//extension Regex to validate password, email, phone number
+extension String{
+    var isValidEmail: Bool{
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailTest.evaluate(with: self)
+    }
+    var isValidPhoneNumber: Bool{
+        let phoneRegex = "[0-9]{10}"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+        return phoneTest.evaluate(with: self)
+    }
+    var isValidPassword: Bool{
+        let passRegex = "[A-Za-z0-9_@!#$%^&*_-]{8,}"
+        let passTest = NSPredicate(format: "SELF MATCHES %@", passRegex)
+        return passTest.evaluate(with: self)
     }
 }
